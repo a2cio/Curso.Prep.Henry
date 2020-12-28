a2f317cf79a6ef75d8bbf3a4f04a42168d5d4e41
a2cio <conanto@gmail.com>
2020-12-27 22:45:17 -0500

    Agregamos 07-JS-VI/homework/conceptos.txt y actualizamos 07-JS-VI/homework/homework.js

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -32,10 +32,10 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
 
-  let nuevoArray = [];
-  array.forEach(function(elemento){
-    nuevoArray.push(cb(elemento));
-  });
+  const nuevoArray = array.map(function(elemento){
+    return cb(elemento);
+  })
+
   return nuevoArray;
 }
 
